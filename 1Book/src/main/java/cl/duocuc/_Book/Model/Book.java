cc4a06b065ce2cf7d9c3aaab21df8976e2661551
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-09 18:02:20 -0400

    se corrigio la estructura de los directorios de resource,el application properties y el .xml. Y  ahora si corre el programa y pudo crear la tabla book en laragon

--- 1Book/src/main/java/cl/duocuc/_Book/Model/Book.java
+++ 1Book/src/main/java/cl/duocuc/_Book/Model/Book.java
@@ -1,9 +1,6 @@
 package cl.duocuc._Book.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,7 @@ public class Book {
     private Long isbn;
     private String title;
     private String author;
+    @Column(name = "year_publicated")
     private Integer yearPublicated;
     private Integer stock;
 }
