e91cb0a5c8d9fec7043b9c8ec12a3f23d732b7fa
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-10 02:13:31 -0400

    controller listo, estaria faltando probar en postman noma.

---
+++ 1Book/src/main/java/cl/duocuc/_Book/Controller/BookController.java
@@ -0,0 +1,60 @@
+package cl.duocuc._Book.Controller;
+
+import cl.duocuc._Book.DTO.BookDTO;
+import cl.duocuc._Book.Service.IBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final IBookService service;
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping("/{isbn}")
+    public ResponseEntity<BookDTO> findByIsbn(@PathVariable Long isbn) {
+        BookDTO book = service.findByIsbn(isbn);
+        if(book != null) {
+            return ResponseEntity.ok(book);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscar/{title}")
+    public ResponseEntity<BookDTO> findByTitle(@RequestParam String title) {
+        BookDTO book = service.findByTitle(title);
+        if(book != null) {
+            return ResponseEntity.ok(book);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDTO> save(@RequestBody BookDTO bookDTO) {
+        BookDTO save = service.save(bookDTO);
+        if(save != null) {
+            return new ResponseEntity<>(service.save(bookDTO), HttpStatus.CREATED);
+        }
+    }
+
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<Void> delete(@PathVariable Long isbn) {
+        service.deleteByIsbn(isbn);
+        return ResponseEntity.noContent().build();
+    }
+}
